17e272d99ae22d7f0f946c9a24cbc9a4d4ad071d
dnedaniele <esp.dne@gmail.com>
2020-04-22 16:27:24 +0200

    bmi

--- javaScriptBasics/index.js
+++ javaScriptBasics/index.js
@@ -207,3 +207,20 @@ function calcWeapons(numMen){
 
 
 calcEquipment(100);
+
+// BMI CALCULATOR
+//The first parameter should be the weight and the second should be the height.
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+var bmi = bmiCalculator(65, 1.8); 
+bmi should equal 20 when it's rounded to the nearest whole number.
+*/
+
+function bmiCalculator(yourWeight, yourHeight){
+  var bmiNum = yourWeight/ (Math.pow(yourHeight, 2));
+  return bmiNum;
+}
+
+
+var bmi = bmiCalculator(60, 1.7); 
+console.log(bmi);
+
