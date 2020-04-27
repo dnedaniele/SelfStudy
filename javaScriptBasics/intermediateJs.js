411a338e04d90d83a31d032daa66473602632b52
dnedaniele <esp.dne@gmail.com>
2020-04-27 22:45:17 +0200

    99 bottles of beer on the wall

--- javaScriptBasics/intermediateJs.js
+++ javaScriptBasics/intermediateJs.js
@@ -146,7 +146,27 @@ function fizzBuzz() {
           output.push(count);
         }
     count++;
-    
+
     }
-   
-    console.log(output);
+   console.log(output);
+}
+
+// 99 Bottles of beer on the wall
+
+var rhyme = [];
+ var count = 99;
+
+function beer() {
+
+    while (count > 1) {
+
+    rhyme.push(count + " bottles of beer on the wall, " + count +" bottles of beer.");
+    count--;
+    rhyme.push("Take one down and pass it around, " + count + " bottles of beer on the wall.")
+    }
+
+    rhyme.push("1 bottle of beer on the wall, 1 bottle of beer.Take one down and pass it around, no more bottles of beer on the wall.No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall.");
+    
+   console.log(rhyme);
+    
+}
