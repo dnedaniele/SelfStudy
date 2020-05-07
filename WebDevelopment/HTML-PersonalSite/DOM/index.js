246b6e6d22fd4b699a0cf214c233c4f4d36183c9
dnedaniele <esp.dne@gmail.com>
2020-05-07 20:10:01 +0200

    domnew

--- WebDevelopment/HTML-PersonalSite/DOM/index.js
+++ WebDevelopment/HTML-PersonalSite/DOM/index.js
@@ -1,1 +1,11 @@
-document.query
+document.querySelector("h1").classList.add("huge");
+
+document.querySelector("input").innerHTML
+
+document.querySelector("input").innerHTML;
+
+document.querySelector("button").textContent = "baciami";
+
+document.querySelector("ul").lastElementChild;
+
+document.querySelector("ul").lastElementChild.innerHTML = "<strong> Fuck >/strong>";
