04877c87a0a4e51d933d163500131bbb831f9b07
dnedaniele <esp.dne@gmail.com>
2020-05-11 19:29:34 +0200

    try

--- dom_advanced/index.js
+++ dom_advanced/index.js
@@ -1,10 +1,57 @@
-function diceThrow1Player() {
+
+//// Function Player 1
+
+function diceThrow1Player() {  
   var randomNumber1 = Math.floor(Math.random() * 7);
 
-  if (randomNumber1 === 1) {
-  }
+if (randomNumber1 === 1 ) {
+    document.querySelector(".dice .img1").setAttribute("src", "images/dice1.png");
+}
+if (randomNumber1 === 2) {
+  document.querySelector(".dice .img1").setAttribute("src", "images/dice2.png");
+}
+if (randomNumber1 === 3) {
+  document.querySelector(".dice .img1").setAttribute("src", "images/dice3.png");
+}
+if (randomNumber1 === 4) {
+  document.querySelector(".dice .img1").setAttribute("src", "images/dice4.png");
+}
+if (randomNumber1 === 5) {
+  document.querySelector(".dice .img1").setAttribute("src", "images/dice5.png");
+}
+if (randomNumber1 === 6) {
+  document.querySelector(".dice .img1").setAttribute("src", "images/dice6.png");
+}
 }
 
-function clickChange() {
+////////// Function Player 2
+
+function diceThrow2Player() {  
+  var randomNumber2 = Math.floor(Math.random() * 7);
+
+if (randomNumber2 === 1 ) {
+    document.querySelector(".dice .img2").setAttribute("src", "images/dice1.png");
+}
+if (randomNumber2 === 2) {
+  document.querySelector(".dice .img2").setAttribute("src", "images/dice2.png");
+}
+if (randomNumber2 === 3) {
+  document.querySelector(".dice .img2").setAttribute("src", "images/dice3.png");
+}
+if (randomNumber2 === 4) {
+  document.querySelector(".dice .img2").setAttribute("src", "images/dice4.png");
+}
+if (randomNumber2 === 5) {
+  document.querySelector(".dice .img2").setAttribute("src", "images/dice5.png");
+}
+if (randomNumber2 === 6) {
+  document.querySelector(".dice .img2").setAttribute("src", "images/dice6.png");
+}
+}
+
+///////
+
+function clickPlay() {
   diceThrow1Player();
+  diceThrow2Player();
 }
