7197d09c583083f028443c96fc61b3e21a73e0b3
Daniele Esposito <esp.dne@gmail.com>
2020-05-11 16:57:19 +0200

    sf

--- WebDevelopment/HTML-PersonalSite/dom_advanced/index.js
+++ WebDevelopment/HTML-PersonalSite/dom_advanced/index.js
@@ -1,9 +1,10 @@
+function diceThrow1Player() {
+  var randomNumber1 = Math.floor(Math.random() * 7);
 
+  if (randomNumber1 === 1) {
+  }
+}
 
-function diceThrow1Player () {
-    var randomNumber1 = Math.floor(Math.random() * 7);
-
-    if (randomNumber1 === 1 ) {
-        
-    }
+function clickChange() {
+  diceThrow1Player();
 }
