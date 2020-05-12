91270ee19bd02a86e2cfb4f5c059335aa397bccb
dnedaniele <esp.dne@gmail.com>
2020-05-12 14:59:21 +0200

    first attempt

--- dom_advanced/index.js
+++ dom_advanced/index.js
@@ -6,7 +6,7 @@
 
 
 function diceThrow1Player() {  
-  var randomNumber1 = Math.floor(Math.random() * 7);
+  var randomNumber1 = Math.floor(Math.random() * 6)+1;
 
 if (randomNumber1 === 1 ) {
     document.querySelector(".dice .img1").setAttribute("src", "images/dice1.png");
@@ -26,14 +26,14 @@ if (randomNumber1 === 5) {
 if (randomNumber1 === 6) {
   document.querySelector(".dice .img1").setAttribute("src", "images/dice6.png");
 }
-//console.log("first player " + randomNumber1);
+console.log("first player " + randomNumber1);
 return randomNumber1
 }
 
 ////////// Function Player 2
 
 function diceThrow2Player() {  
-  var randomNumber2 = Math.floor(Math.random() * 7); 
+  var randomNumber2 = Math.floor(Math.random() * 6)+1; 
 
 if (randomNumber2 === 1 ) {
     document.querySelector(".dice .img2").setAttribute("src", "images/dice1.png");
@@ -53,18 +53,21 @@ if (randomNumber2 === 5) {
 if (randomNumber2 === 6) {
   document.querySelector(".dice .img2").setAttribute("src", "images/dice6.png");
 }
-//console.log("second player " + randomNumber2);
+console.log("second player " + randomNumber2);
 return randomNumber2
 }
 
 ////
-function compareScore() {
+function compareScore(randomNumber1, randomNumber2) {
   
-  if(diceThrow1Player() > diceThrow2Player() ) {
-   console.log("P 1 Wins");
+  if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+  }
+  else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins";
   }
   else {
-    console.log("P 2 Wins");
+    document.querySelector("h1").innerHTML = "Draw";
   }
 
   
@@ -74,8 +77,8 @@ function compareScore() {
 ///////
 
 function clickPlay() {
-  diceThrow1Player();
-  diceThrow2Player();
+ diceThrow1Player();
+ diceThrow2Player();
   compareScore();
 }
 
