fbee32e8048cce98d31ee819e00cc38f8dec7d72
dnedaniele <esp.dne@gmail.com>
2020-06-18 15:36:00 +0200

    commands

--- jquery/index.js
+++ jquery/index.js
@@ -1,1 +1,9 @@
-document.body.innerHTML="ciao bella!"
+
+
+$("h1").addClass("big-title margin-50"); 
+
+$("h1").text("Bye"); //innerText
+
+$("button").html("<em>Hey</em>"); //innerHTML
+
+$("a").attr("href", "https://www.berlin.de/en/")  //change or add attribute
