75f854c51bbf05b57f9c4b4008ceb286ff836331
dnedaniele <esp.dne@gmail.com>
2020-06-30 22:05:38 +0200

    nodeJS

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+//jshint esversion:6
+
+// NODEJS API FILE SYSTEM
+
+//    const fs = require("fs");
+
+//https://nodejs.org/api/fs.html  fs.copyFileSync(src, dest[, mode])
+
+//    fs.copyFileSync("file1.txt","fileCopyOf1.text");
