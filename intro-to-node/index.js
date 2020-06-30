33a9b9c0dad3660f4a8c30bc400ed8bb5409083a
dnedaniele <esp.dne@gmail.com>
2020-06-30 22:51:26 +0200

    villains

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+
 // NODEJS API FILE SYSTEM
 
 //    const fs = require("fs");
@@ -7,3 +8,21 @@
 //https://nodejs.org/api/fs.html  fs.copyFileSync(src, dest[, mode])
 
 //    fs.copyFileSync("file1.txt","fileCopyOf1.text");
+
+
+//NPM SUPERHEROES
+
+   var superheroes = require("superheroes");
+
+  var myHero = superheroes.random();
+
+  console.log("My superhero is " + myHero);
+
+
+// NPM SUPER VILLAINS
+
+var supervillains = require("supervillains");
+
+var mySuperVillains = supervillains.random();
+
+console.log("my super villains is " + mySuperVillains);
