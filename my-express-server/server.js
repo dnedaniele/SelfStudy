ebf1e54faddae566c0ef8e9680e2d3418ef548a7
dnedaniele <esp.dne@gmail.com>
2020-07-06 19:13:40 +0200

    route

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,13 +1,22 @@
 //jshint esversion:6
 
 const express = require("express");
+const { response } = require("express");
 
 const app = express();
 
-app.get("/", function(req, res){
+app.get("/", function(request, response){
 //console.log(request);
-response.send("<h1>Hello World!</h1>");
+response.send("<h1> this is My Hompage - Hello World!</h1>");
 });
+
+app.get("/contact", function(request, response){
+    response.send("<h1>this is my contact list</h1> <br> <ul><li>Telephone<li><li>Adress<li><li>E-mail<li></ul>")
+})
+
+app.get("/about", function(request, response) {
+    response.send("<h1>this is the website of Daniele</h1><br><h2>Daniele is learning ExpressJS</h2>")
+})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
