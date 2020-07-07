2c7599534bfc5e50684264fd7b703d705d57d43f
dnedaniele <esp.dne@gmail.com>
2020-07-07 21:28:00 +0200

    calculator server

--- my-express-server/calculator/calculator.js
+++ my-express-server/calculator/calculator.js
@@ -1,3 +1,20 @@
 const express = require("express");
+const { response } = require("express");
 
 const app = express();
+
+app.get("/", function (req, res) {
+res.send("Hello homePage")
+})
+
+app.get("/contact", function (req, res) {
+    res.send("please contact me to this email: daniele@daniele.com")
+})
+
+app.get("/about", function (req, res) {
+    res.send("<h1>ABOUT this is an example of response</h1>")
+})
+
+app.listen(3500, function() {
+    console.log("Server is running on 3500")
+} )
